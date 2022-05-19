341835ecab7d7f49be71a67c81234850d2efd374
B B <leopardironfist@gmail.com>
2022-05-19 10:23:22 -0600

    cookiecutter ds fmt

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='default cookiecutter ds',
+    author='brian britton',
+    license='',
+)
